e10f56116338a91b489e293e68012cd1d2d5739d
FagnerMoraes <fagm.carvalho@gmail.com>
2022-08-24 20:41:48 -0300

    Inicial commit

---
+++ PrimeService/PrimeService.cs
@@ -0,0 +1,11 @@
+﻿
+using System;
+
+namespace PrimeService;
+public class PrimeService
+{
+    public bool IsPrime(int candidate)
+    {
+        throw new NotImplementedException("Please create a test first.");
+    }
+}
